fa676eb461cf54c8b53d47ff4097cc28495bcb13
Abhishek <abhishekrnc>
2025-01-08 22:49:37 +0530

    Home009,010

---
+++ src/ex_01_Java_Basic/Home009_PrintMSG.java
@@ -0,0 +1,8 @@
+package ex_01_Java_Basic;
+
+public class Home009_PrintMSG {
+    public static void main(String[] args) {
+        String a = "Hi this is Abhishek, and I work as a Test Engineer";
+        System.out.println(a);
+    }
+}
---
+++ src/ex_01_Java_Basic/Home010_longKeyword.java
@@ -0,0 +1,11 @@
+package ex_01_Java_Basic;
+
+public class Home010_longKeyword {
+    public static void main(String[] args) {
+        long a = 1111676762;
+        int b = 33321;
+        long c = a+b;
+        System.out.println(c);
+
+    }
+}
