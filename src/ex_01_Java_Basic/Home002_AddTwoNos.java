0970fa8ad3755dbcc71c84d702b7016e3b0fb2da
Abhishek <abhishekrnc>
2025-01-08 22:24:39 +0530

    Home002

---
+++ src/ex_01_Java_Basic/Home002_AddTwoNos.java
@@ -0,0 +1,10 @@
+package ex_01_Java_Basic;
+
+public class Home002_AddTwoNos {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 40;
+        int c = a+b;
+        System.out.println(c);
+    }
+}
