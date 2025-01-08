c7dff087ddf4cb2e97a41aaa284ce7a54b2a7fdd
Abhishek <abhishekrnc>
2025-01-08 22:33:32 +0530

    Home006

---
+++ src/ex_01_Java_Basic/Home006_AddDecimalNos.java
@@ -0,0 +1,10 @@
+package ex_01_Java_Basic;
+
+public class Home006_AddDecimalNos {
+    public static void main(String[] args) {
+        double a = 10.5;
+        int b = 10;
+        double c = a+b;
+        System.out.println(c);
+    }
+}
