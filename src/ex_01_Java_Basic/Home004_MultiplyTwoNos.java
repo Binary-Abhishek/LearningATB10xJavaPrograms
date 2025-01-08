faf24dd8c38d73a0138f5c5e0e3cd3e98d3c442a
Abhishek <abhishekrnc>
2025-01-08 22:31:39 +0530

    Home003,4,5 push

---
+++ src/ex_01_Java_Basic/Home004_MultiplyTwoNos.java
@@ -0,0 +1,10 @@
+package ex_01_Java_Basic;
+
+public class Home004_MultiplyTwoNos {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 3;
+        int c = a * b;
+        System.out.println(c);
+    }
+}
---
+++ src/ex_01_Java_Basic/Home005_DivideTwoNos.java
@@ -0,0 +1,10 @@
+package ex_01_Java_Basic;
+
+public class Home005_DivideTwoNos {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 2;
+        int c = a/b;
+        System.out.println(c);
+    }
+}
