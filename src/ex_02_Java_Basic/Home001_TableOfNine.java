ae065cebe0a690210ba0c6fe6d8b3ce25fd5a04f
Abhishek <abhishekrnc>
2025-01-14 21:25:06 +0530

    9 table push gsdfg

--- src/ex_02_Java_Basic/Home001_TableOfNine.java
+++ src/ex_02_Java_Basic/Home001_TableOfNine.java
@@ -1,4 +1,26 @@
 package ex_02_Java_Basic;
 
 public class Home001_TableOfNine {
+    public static void main(String[] args) {
+        int a = 9;
+        System.out.printf("9 x 1 = %d", a*1);
+        System.out.println();
+        System.out.printf("9 x 1 = %d", a*2);
+        System.out.println();
+        System.out.printf("9 x 1 = %d", a*3);
+        System.out.println();
+        System.out.printf("9 x 1 = %d", a*4);
+        System.out.println();
+        System.out.printf("9 x 1 = %d", a*5);
+        System.out.println();
+        System.out.printf("9 x 1 = %d", a*6);
+        System.out.println();
+        System.out.printf("9 x 1 = %d", a*7);
+        System.out.println();
+        System.out.printf("9 x 1 = %d", a*8);
+        System.out.println();
+        System.out.printf("9 x 1 = %d", a*9);
+        System.out.println();
+        System.out.printf("9 x 1 = %d", a*10);
+    }
 }
