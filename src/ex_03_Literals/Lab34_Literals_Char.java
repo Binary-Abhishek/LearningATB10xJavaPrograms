288e74808f984360c2fe6665533da93c74667a64
Abhishek <abhishekrnc>
2025-01-24 00:37:05 +0530

    Home009,010

--- src/ex_03_Literals/Lab34_Literals_Char.java
+++ src/ex_03_Literals/Lab34_Literals_Char.java
@@ -3,5 +3,25 @@ package ex_03_Literals;
 public class Lab34_Literals_Char {
     public static void main(String[] args) {
 
+        //Char - store
+        char c1 = 'A';
+        //char c2 = "A" //String - bunch o char (multiple chars)
+
+        char c2 ='B';
+        char c3 = '@';
+        char c4 = '_';
+        char c5 = '9';
+        char c6 = '1';
+
+        //Escape Char
+        char new_line = '\n';
+
+        System.out.println("Abhishek" + new_line + "Kumar");
+
+        char c10 = 'A';
+        //ASCII, (limited numbers) - A --> 65
+        //UNICODE (Ijndia, Jap, China) --> Rupees -
+
+
     }
 }
