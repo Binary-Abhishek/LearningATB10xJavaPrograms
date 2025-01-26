f1d2bc84780f107680d994ac7768c9f022af13da
Abhishek <abhishekrnc>
2025-01-27 00:07:35 +0530

    Strings function

---
+++ src/ex_13_Functions/Lab124.java
@@ -0,0 +1,8 @@
+package ex_13_Functions;
+
+public class Lab124 {
+    public static void main(String[] args) {
+        int result = Math.max(30,45);
+        System.out.println(result);
+    }
+}
---
+++ src/ex_13_Functions/Lab125.java
@@ -0,0 +1,15 @@
+package ex_13_Functions;
+
+import java.util.Scanner;
+
+public class Lab125 {
+    public static void main(String[] args) {
+        Math.max(30,34);
+        Integer.parseInt("30");
+
+        String.valueOf(30);
+        Scanner sc = new Scanner(System.in);
+        sc.nextInt();
+
+    }
+}
