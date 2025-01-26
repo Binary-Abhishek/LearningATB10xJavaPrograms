d7bc46ea2bde659614bc87547d64b92605932b88
Abhishek <abhishekrnc>
2025-01-26 22:37:43 +0530

    Home009,010

---
+++ src/ex_14_Strings/Lab133.java
@@ -0,0 +1,9 @@
+package ex_14_Strings;
+
+public class Lab133 {
+    public static void main(String[] args) {
+        String s1 = "Abhishek";
+        s1 = s1.concat("Kumar");
+        System.out.println(s1);
+    }
+}
